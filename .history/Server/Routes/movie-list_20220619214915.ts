28fcb5c8c434f28f8983f5ad42e197eebceacb57
PHLin199988 <yk40403@gmail.com>
2022-06-19 22:42:27 -0400

    initial commit

---
+++ .history/Server/Routes/movie-list_20220619214915.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const router = express.Router();
+
+import { AuthGuard } from '../Util';
+
+import { DisplayMovieList } from '../Controllers/movie-list';
+
+// Display Movie List Page
+router.get('/movie-list', AuthGuard, DisplayMovieList);
+
+// Display Add Page
+
+// Display Edit Page
+
+// Process Edit Page
+
+// Process Delete Page
+export default router;
---
+++ .history/Server/Routes/movie-list_20220619220033.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+const router = express.Router();
+
+import { AuthGuard } from '../Util';
+
+import { DisplayAddPage, DisplayEditPage, DisplayMovieList, ProcessAddPage, ProcessDeletePage, ProcessEditPage } from '../Controllers/movie-list';
+
+// Display Movie List Page
+router.get('/movie-list', AuthGuard, DisplayMovieList);
+
+// Display Add Page
+router.get('/add', AuthGuard, DisplayAddPage);
+
+// Display Edit Page
+router.get('/edit/:id', AuthGuard, DisplayEditPage);
+
+// Process Add Page
+router.post('/add', AuthGuard, ProcessAddPage);
+
+// Process Edit Page
+router.post('/edit/:id', AuthGuard, ProcessEditPage);
+
+// Process Delete Page
+router.get('/delete/:id', AuthGuard, ProcessDeletePage);
+
+
+export default router;
